db7ff5db16b828e7eae865bf4ca470e47901ccae
AndrewHanasiro <andrewkhanasiro@gmail.com>
2022-11-04 19:20:26 -0300

    feat: add mockall
    add more test
    
    chore: testing working
    
    fix: GH action
    
    test: GH action
    
    fix: typo
    
    test: 2
    
    test: 3
    
    test: 4
    
    test: 6
    
    test: 7
    
    test: 8
    
    test: 9
    
    test: 10
    
    test: 11

--- src/core/usecase/invoice_usecase.rs
+++ src/core/usecase/invoice_usecase.rs
@@ -1,3 +1,5 @@
+use uuid::Uuid;
+
 use crate::core::{
     dto::invoice::Invoice,
     usecase::driven::reading_invoice::{ReadingInvoice, ReadingInvoiceError},
@@ -10,14 +12,21 @@ pub struct InvoiceUsecase {
 }
 
 impl InvoiceUsecase {
-    pub async fn get_by_user_id(&self, user_id: &str) -> Result<Vec<Invoice>, String> {
-        let result = self.reading_user.list_by_id(user_id).await;
+    pub async fn get_by_user_id(&self, external_user_id_str: &str) -> Result<Vec<Invoice>, String> {
+        let user_id = match Uuid::parse_str(external_user_id_str) {
+            Ok(id) => id,
+            Err(_error) => return Err(String::from("external id provided isn't uuid")),
+        };
+        let result = self.reading_user.list_by_id(&user_id).await;
         let user = match result {
             Ok(user) => user,
             Err(error) => match error {
                 ReadingUserError::UserNotFoundError => return Err(String::from("User Not found")),
                 ReadingUserError::UnmappedError => {
                     return Err(String::from("Something wrong happen"))
+                }
+                ReadingUserError::InvalidUuidError => {
+                    return Err(String::from("Database System error"))
                 }
             },
         };
@@ -28,6 +37,63 @@ impl InvoiceUsecase {
                 ReadingInvoiceError::InvoiceNotFoundError => Err(String::from("Invoice Not found")),
                 ReadingInvoiceError::UnmappedError => Err(String::from("Something wrong happen")),
             },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::InvoiceUsecase;
+    use crate::core::{
+        dto::{invoice::Invoice, user::User},
+        usecase::driven::{reading_invoice::MockReadingInvoice, reading_user::MockReadingUser},
+    };
+    use mockall::predicate;
+    use uuid::Uuid;
+
+    #[actix_rt::test]
+    async fn should_succeed_listing_invoice_by_user() {
+        let id = Uuid::new_v4();
+        let external_id = Uuid::new_v4();
+        let user = User {
+            id,
+            external_id: Uuid::new_v4(),
+        };
+        let invoices = vec![Invoice {
+            id: Uuid::new_v4(),
+            status: String::from("pending"),
+            user_id: id,
+        }];
+        let mut mock_ru = MockReadingUser::new();
+        mock_ru
+            .expect_list_by_id()
+            .with(predicate::eq(external_id))
+            .times(1)
+            .return_const(Ok(user.clone()));
+        let mut mock_ri = MockReadingInvoice::new();
+        mock_ri
+            .expect_list_by_user_id()
+            .with(predicate::eq(user.id))
+            .times(1)
+            .return_const(Ok(invoices.clone()));
+
+        let invoice_usecase = InvoiceUsecase {
+            reading_user: Box::new(mock_ru),
+            reading_invoice: Box::new(mock_ri),
+        };
+        let result = invoice_usecase
+            .get_by_user_id(&external_id.to_string())
+            .await;
+
+        match result {
+            Ok(resp) => {
+                let inv = invoices[0].clone();
+                assert_eq!(inv.id, resp[0].id);
+                assert_eq!(inv.status, resp[0].status);
+                assert_eq!(inv.user_id, resp[0].user_id);
+            }
+            Err(error) => panic!("Test wen wrong: {}", error),
         }
     }
 }
