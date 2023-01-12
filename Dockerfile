18e8c29c8e8a6a22d86b69d1f5438f5d8586f5d1
AndrewHanasiro <andrewkhanasiro@gmail.com>
2023-01-11 22:20:48 -0300

    feat: add kafka and topics

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,14 @@ WORKDIR /app
 RUN apt-get update && apt-get -y install cmake protobuf-compiler libssl-dev pkg-config
 COPY . .
 RUN cargo build --release
+RUN cargo build --release --bin  kafka
 
 FROM ubuntu:22.04 as deploy
 WORKDIR /app
 COPY --from=dependency /app/target/release/http /app
+COPY --from=dependency /app/target/release/kafka /app
 RUN apt-get update && apt-get -y install wget
 RUN wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
 
 RUN dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
 EXPOSE 5002
-CMD [ "./http" ]
--- src/config/kafka.rs
+++ src/config/kafka.rs
@@ -1,35 +1,24 @@
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
-use rdkafka::consumer::{BaseConsumer, Consumer};
-use rdkafka::producer::FutureProducer;
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{Consumer, DefaultConsumerContext};
 
-pub fn get_consumer(topics: &[&str]) -> BaseConsumer {
+// A type alias with your custom consumer can be created for convenience.
+pub type LoggingConsumer = StreamConsumer<DefaultConsumerContext>;
+
+pub fn get_consumer(topics: &[&str]) -> LoggingConsumer {
     let config = super::env_var::get_config();
-
-    let consumer: BaseConsumer = ClientConfig::new()
-        .set("group.id", "billing_consumer")
+    let context = DefaultConsumerContext;
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", "auth-plus-authentication")
         .set("bootstrap.servers", &config.kafka.url)
         .set("enable.partition.eof", "false")
         .set("session.timeout.ms", "6000")
         .set("enable.auto.commit", "true")
         .set_log_level(RDKafkaLogLevel::Debug)
-        .create()
+        .create_with_context(context)
         .expect("Consumer creation failed");
-
     consumer
-        .subscribe(topics)
+        .subscribe(&topics.to_vec())
         .expect("Can't subscribe to specified topics");
-
     consumer
-}
-
-pub fn get_producer() -> FutureProducer {
-    let config = super::env_var::get_config();
-
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", &config.kafka.url)
-        .set("message.timeout.ms", "5000")
-        .create()
-        .expect("Producer creation error");
-
-    producer
 }
